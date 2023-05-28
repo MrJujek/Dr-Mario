5adda85dcd514a84c8db3a4a83018e1f9e4bba41
MrJujek <julian.dworzycki@gmail.com>
2023-05-28 20:20:28 +0200

    docs: :memo: main documentation

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import "../scss/main.scss";
 
 let game: Game;
 
-let interval = setInterval(() => {
+/**
+ * @description This interval is used to animate the doctor
+ * @type {number}
+ * @default
+ */
+export let interval = setInterval(() => {
     if ((document.getElementById("doctor") as HTMLElement).style.backgroundImage == 'url("../img/doctor/doctor_2.png")') {
         (document.getElementById("doctor") as HTMLElement).style.backgroundImage = "url(../img/doctor/doctor_1.png)";
     } else {
@@ -11,14 +16,27 @@ let interval = setInterval(() => {
     }
 }, 800);
 
+/**
+ * @description This event is used to animate the start button
+ * @type {HTMLElement}
+ * @default
+ */
 (document.getElementById("start") as HTMLElement).onmousedown = () => {
     (document.getElementById("start") as HTMLElement).style.backgroundImage = "url(../img/start_pressed.png)";
 }
 
+/**
+ * @description This event is used to animate the start button
+ * @returns {void}
+ */
 document.body.onmouseup = () => {
+    if (!document.getElementById("start")) return;
     (document.getElementById("start") as HTMLElement).style.backgroundImage = "url(../img/start.png)";
 }
 
+/**
+ * @description This event is used to start the game
+ */
 (document.getElementById("start") as HTMLElement).onmouseup = () => {
     clearInterval(interval);
     startGame();
@@ -26,7 +44,11 @@ document.body.onmouseup = () => {
     (document.getElementById("start") as HTMLElement).remove();
 }
 
-function startGame() {
+/**
+ * @description This function is used to start the game
+ * @returns {void}
+ */
+export function startGame() {
     game = new Game();
     game.start();
 }
